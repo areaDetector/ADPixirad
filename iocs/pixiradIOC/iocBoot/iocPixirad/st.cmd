a6274756db14b6a8c7f3b98ec9bd1b328bc7152c
Mark Rivers <rivers@cars.uchicago.edu>
2014-02-20 12:34:30 -0600

    Removed D=cam1:, obsolete

--- iocs/pixiradIOC/iocBoot/iocPixirad/st.cmd
+++ iocs/pixiradIOC/iocBoot/iocPixirad/st.cmd
@@ -44,4 +44,4 @@ set_requestfile_path("$(ADPIXIRAD)/pixiradApp/Db")
 iocInit()
 
 # save things every thirty seconds
-create_monitor_set("auto_settings.req", 30,"P=$(PREFIX),D=cam1:")
+create_monitor_set("auto_settings.req", 30,"P=$(PREFIX)")
