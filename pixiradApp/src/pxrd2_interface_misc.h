426482f92e89c96c966a20f220b79c642dcf5fe8
Mark Rivers <rivers@cars.uchicago.edu>
2016-08-17 18:22:45 -0500

    Stripped down to compile without error on Linux and Windows

--- pixiradApp/src/pxrd2_interface_misc.h
+++ pixiradApp/src/pxrd2_interface_misc.h
@@ -35,22 +35,22 @@ extern int verbose;
 #define SYS_TAG_STR "SYS"
 /**************************************************************/
 
-#include <windows.h>
-#include <Windef.h>
-#include <winsock2.h>
-#include "pthread.h"
-#include "BoxUtilities_v2.h"
-#include "interface_command_utilities.h"
-#include "raw_image_correction_module.h"
-#include <stdio.h>
-#include "build_ver.h"
+//#include <windows.h>
+//#include <Windef.h>
+//#include <winsock2.h>
+//#include "pthread.h"
+//#include "BoxUtilities_v2.h"
+//#include "interface_command_utilities.h"
+//#include "raw_image_correction_module.h"
+//#include <stdio.h>
+//#include "build_ver.h"
 
 /**************************************************************/
-typedef enum EnState{DISABLED,ENABLED};
-typedef enum ConnectionState{FREE,ESTABILISHED,CLOSING};
+enum EnState{DISABLED,ENABLED};
+enum ConnectionState{FREE,ESTABILISHED,CLOSING};
 /**************************************************************/
 
-typedef enum ImageType{FULL_RAW,FULL_BMP,DOWNSAMPLED_RAW,DOWNSAMPLED_BMP};
+enum ImageType{FULL_RAW,FULL_BMP,DOWNSAMPLED_RAW,DOWNSAMPLED_BMP};
 
 
 //typedef struct{
@@ -61,9 +61,9 @@ typedef enum ImageType{FULL_RAW,FULL_BMP,DOWNSAMPLED_RAW,DOWNSAMPLED_BMP};
 //}IMAGE;
 
 /**************************************************************/
-typedef enum npi_mode_t{NONPI, NPI,NPISUM};
-typedef enum RUNMODE{TWO_COLS,ONE_COL0,ONE_COL1,ONE_COL_DTF,TWO_COLS_DTF,UNDEFINED};
-typedef enum HVType{AUTOHV,STDHV};
+enum npi_mode_t{NONPI, NPI,NPISUM};
+enum RUNMODE{TWO_COLS,ONE_COL0,ONE_COL1,ONE_COL_DTF,TWO_COLS_DTF,UNDEFINED};
+enum HVType{AUTOHV,STDHV};
 
 typedef struct{
 
@@ -77,11 +77,11 @@ typedef struct{
 
 
 /**************************************************************/
-typedef enum DETECTOR_BUILD{PX1,PX2,PX4,PX8};
-typedef enum ROSCHEMA_t {MONO0,MONO1, MONO2, MONO3, MONO4, MONO5, MONO6, MONO7, DEFAULT} ;
-typedef enum ASIC_TYPE{PII,PIII};
-typedef enum HYBRID_TYPE{CDTE,GAAS};
-typedef enum PIXEL_ARRANGEMENT{EXAGON,SQUARE};
+enum DETECTOR_BUILD{PX1,PX2,PX4,PX8};
+enum ROSCHEMA_t {MONO0,MONO1, MONO2, MONO3, MONO4, MONO5, MONO6, MONO7, DEFAULT} ;
+enum ASIC_TYPE{PII,PIII};
+enum HYBRID_TYPE{CDTE,GAAS};
+enum PIXEL_ARRANGEMENT{EXAGON,SQUARE};
 typedef struct{
 	unsigned short *ptr;
 	int depth;
@@ -162,61 +162,6 @@ enum DataTransferType{OLD_DIFFERENT_SOCKET,NEW_SAME_SOCKET,NEW_DIFFERENT_SOCKET}
 enum FileSaveType{SAVE_OFF,SAVE_OLD_STYLE,SAVE_NEW_STYLE,SAVE_GLORIA_PX1_OLD_STYLE};
 enum Processing_t {DEFAULT_PROCESSING, CORRECTIONS, EX_TO_SQ_MAP_SP, NO_PROCESSING};
 
-typedef struct{
-	RunState 			State;
-	RunType 			Type;
-	EnState 			FlatEn;
-	DataTransferType 	TrasferMode;
-	int 				MonitorRefresh;
-	FileSaveType		SaveMode;
-	int					FileSavingEnabled;
-	char 				filetosave_name[MAX_FILENAME_STR_LENGTH];
-	char 				FF_filetosave_name[MAX_FILENAME_STR_LENGTH];
-	char 				FF_filetoread_name[MAX_FILENAME_STR_LENGTH];
-	pthread_mutex_t 	RunMutex;
-	FILE* 				DataFilePtr;
-	FILE*				FlatFilePtr;
-	Processing_t		Processing_options;
-	int					filter_radius;
-	float 				filter_stdev;
-	EnState 			MultipleDataFile;
-	EnState				AdvancedMode;
-	EnState				SaveInterpolatedData;
-	EnState				SaveInterpolatedDataTiff;
-	EnState				SendVariableDimData;
-}RUN;
-/*************************************************************/
-
-typedef struct{
-	struct sockaddr_in 	client_sockaddr;
-	SOCKET 				client_sock;
-	int 				client_sockadd_len;
-	pthread_t  			ConnectionThread_tid;
-	pthread_t  			UDPThread_tid;
-	pthread_t  			PROCThread_tid;
-	pthread_t  			UDPSupplierThread_tid;
-	int 				Index;
-	ConnectionState 	State;
-	pthread_mutex_t 	socket_rx_mutex;
-	pthread_mutex_t 	socket_tx_mutex;
-	pthread_mutex_t 	self_mutex;
-	char* 				RXBuffer;
-	char* 				TXBuffer;
-	int 				RXMsgLen;
-	int 				TXMsgLen;
-	char 				filetosave_folder[MAX_FILENAME_STR_LENGTH];
-	char 				FF_filetosave_folder[MAX_FILENAME_STR_LENGTH];
-	char 				FF_filetoread_folder[MAX_FILENAME_STR_LENGTH];
-	char 				default_folder[MAX_FILENAME_STR_LENGTH];
-	char 				offline_datafname[MAX_FILENAME_STR_LENGTH];
-	char 				offline_flatfname[MAX_FILENAME_STR_LENGTH];
-	char 				currentdir_abs_path[MAX_FILENAME_STR_LENGTH];
-	RUN					Run;
-	FRAME_PROP			Frame;
-	SENSOR			 	Sens;
-	BOX					ConnBox;
-	CorrList			CorrectionList;
-}Connection;
 
 /**************************************************************/
 typedef struct{
@@ -247,59 +192,14 @@ typedef struct{
 
 #define FF_MAP_LIST_LEN 500
 
-typedef enum RegType{REG0,REG1,REG01};
+enum RegType{REG0,REG1,REG01};
 
-typedef struct{
-	char filename[MAX_PATH];
-	RegType Reg;
-	RUNMODE RunMode;
-	int frame_width;
-	int frame_height;
-	int Asic;
-	int Build;
-	int Hybrid;
-}FFMapRecord;
 
-typedef struct{
-	FFMapRecord Records[FF_MAP_LIST_LEN];
-	int entries;
-}FFMapList;
 /**************************************************************/
-void SetRunState(RUN* Run_ptr,RunState State);
-RunState GetRunState(RUN* Run_ptr);
 void StrRunState(RunState State,char* deststr,int maxlen);
 
 /**************************************************************/
 RUNMODE getRunModeFromStr(const char*);
 /**************************************************************/
-int send_int_over_the_net(SOCKET data_socket,int data);
-int ReadDataFromSocket(Connection* Conn_ptr);
-int WriteDatatoSocket(Connection * Conn_ptr, char* str_buffer, int str_len, char* binary_buffer,int binary_len);
-/**************************************************************/
-int update_run_sett(command incoming_cmd, Connection* Conn_ptr);
-/**************************************************************/
-int createFileList(FileList *List);
-int FreeFileList(FileList *List);
-int file_list_add_record(FileList* List_ptr,const char *char_ptr);
-/**************************************************************/
-int edit_map_sett_from_file(FFMapRecord *MapListRec_ptr,const char* filename);
-int edit_frame_sett_from_file(FFMapRecord *MapListRec_ptr,const char* filename);
-int populate_ffmap_list(FFMapList* MapList,const char* dir,const char* ext);
-int populate_frame_list(FFMapList* MapList,const char* dir,const char* ext);
-int file_check_extension(const char* filename,const char* ext);
-/**************************************************************/
-int stringfy_ffmap_list(FFMapList MapList,char* str,int strlen);
-int stringfy_frame_list(FFMapList MapList,char* str,int str_len);
-/**************************************************************/
-int create_folders(Connection* Conn_ptr,char*data_path,char* flat_towrite_path,char* flat_toread_path, char* default_path,int strlen);
-int edit_sensor_properties(Connection* Conn_ptr,SENSOR Sensor);
-int update_sensor_sett_from_cmd(BOX * temp_box,command incoming_cmd, Connection* Conn_ptr);
-int update_file_saving_style_from_cmd(command incoming_cmd, Connection* Conn_ptr);
-/*************************************************************/
-int apply_special_configurations_tobox(BOX * temp_box,SENSOR Sens);
-/*************************************************************/
-int get_crop_from_cmd(command incoming_cmd, Connection*Conn);
-/*************************************************************/
-int get_interpol_options_from_cmd(command incoming_cmd, Connection*Conn);
 
 #endif /* PXRD2_INTERFACE_MISC_H_ */
